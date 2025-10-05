49f8e6268b4f91e3c2eb96710b5a0bf738d7c801
Guilherme Borges <guilhermeborgessilva04@gmail.com>
2025-10-05 13:06:51 -0300

    atualiza componentes e páginas

--- projeto-eventup/src/components/Footer.jsx
+++ projeto-eventup/src/components/Footer.jsx
@@ -3,7 +3,8 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>&copy; 2024 EventUp. Todos os direitos reservados.</p>
+      <p>&copy; 2026 EventUp. Todos os direitos reservados.</p>
     </footer>
   );
 }
+
